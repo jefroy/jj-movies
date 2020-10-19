95d1dde4413360b338e592c5c79894f915d51e75
Ajay S <ajay_sieunarine@hotmail.com>
2020-10-18 22:33:23 -0400

    wip/ implement pop up trailers

--- src/Row/Row.js
+++ src/Row/Row.js
@@ -3,6 +3,9 @@ import axios from '../API/axios'
 import requests from "../API/requests";
 import "./Row.css";
 
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
+
 function Row({title, fetchUrl, isLargeRow}) {
   // states (react variables)
   const [movies, setMovies] = useState([]);
@@ -21,7 +24,36 @@ function Row({title, fetchUrl, isLargeRow}) {
     fetchData();
   }, [fetchUrl]);
 
-  // console.log(movies);
+  // youtube player stuff
+  const [trailerUrl, setTrailerUrl] = useState('');
+  const opts = { // options
+      height: '390',
+      width: '100%',
+      playerVars: {
+          // dev google youtube player params
+          autoplay: 1,
+      },
+  };
+
+  const handleClick = (movie) => {
+      if(trailerUrl){ // if the video is open, close it
+          setTrailerUrl('');
+      } else{
+          console.log('movie name: ', movie.name);
+          movieTrailer(movie?.name || "")
+              .then((url) => {
+                  // https://www.youtube.com/watch?v=XTZX9psWJ9o
+                  const urlParams = new URL(url).search; // search will give us everything after the '?'
+                  console.log('urlParams: ', urlParams);
+                  const urlSearchParams = new URLSearchParams(urlParams); // allow us to use .get
+                  console.log('urlSearchParams.get(\'v\'): ', urlSearchParams.get('v'));
+                  setTrailerUrl(urlSearchParams.get('v')); // gets the VALUE of the query, so ?v= VALUE
+              })
+              .catch((error) =>
+                  console.log(error, trailerUrl)
+              );
+      }
+  };
 
   return (
     <div className={'row'}>
@@ -35,6 +67,7 @@ function Row({title, fetchUrl, isLargeRow}) {
             // poster_path = "/akdopakwpodkap.jpb"
             <img
                 key={movie.id} // unique identifier for effectively rendering changes
+                onClick={() => handleClick(movie)}
                 className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
                 src={`
                   ${requests.BASE_POSTER_PATH}${
@@ -46,7 +79,8 @@ function Row({title, fetchUrl, isLargeRow}) {
         ))}
       </div>
 
-      {/*  container -> posters*/}
+      {/*  show video if url is valid*/}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
 
     </div>
   )
